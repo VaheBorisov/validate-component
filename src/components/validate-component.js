b69c5eb68d93efe4547fe5e0979ad204284c1145
Vahe <borisov.vahe@mail.ru>
2021-04-07 20:18:42 +0400

    update validate method

--- src/components/validate-component.js
+++ src/components/validate-component.js
@@ -3,20 +3,26 @@ import React, { Component } from 'react';
 export default class ValidateComponent extends Component {
 
     static validate = (component) => {
-        const { componentId, children } = component.props
-        console.log(componentId)
-        React.Children.forEach(children, (child) => {
+        const { componentId, children } = component.props;
+        console.log(componentId);
+        React.Children.forEach(children, child => {
+            const childsChildren = React.Children.toArray(child.props.children)
+            if (child.type.prototype.constructor !== component.__proto__.constructor && childsChildren.length !== 0) {
+                childsChildren.forEach(child => {
+                    ValidateComponent.validate(child)
+                })
+            }
             if (child.type.prototype.constructor === component.__proto__.constructor) {
                 ValidateComponent.validate(child)
             }
-        })
-    }
+        });
+    };
 
     render() {
         const { componentId } = this.props;
         return (
             <>
-                <h1 onClick={() => ValidateComponent.validate(this)}>{componentId}</h1>
+                <h1 onClick={() => ValidateComponent.validate(this)} className="validateComponent">ValidateComponent {componentId}</h1>
                 <div>{this.props.children}</div>
             </>
         )
